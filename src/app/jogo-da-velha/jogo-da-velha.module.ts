ba4df5cb203e4472a931b0e4c87721a50c12e3c3
marcos mol <marcos_mol@hotmail.com>
2020-01-03 22:38:48 -0300

    jogo daa velha

--- src/app/jogo-da-velha/jogo-da-velha.module.ts
+++ src/app/jogo-da-velha/jogo-da-velha.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JogoDaVelhaComponent } from './jogo-da-velha.component';
+import { JogoDaVelhaService } from './shared';
 
 
 
@@ -11,6 +12,9 @@ import { JogoDaVelhaComponent } from './jogo-da-velha.component';
   ],
   exports: [
     JogoDaVelhaComponent
+  ],
+  providers: [
+    JogoDaVelhaService
   ]
 })
 export class JogoDaVelhaModule { }
