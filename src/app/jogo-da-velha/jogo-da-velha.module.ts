e4a079bc8c8a89a2751b6cb7935b0359620b3b05
marcos mol <marcos_mol@hotmail.com>
2020-01-03 22:02:19 -0300

    primeiro commit

---
+++ src/app/jogo-da-velha/jogo-da-velha.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { JogoDaVelhaComponent } from './jogo-da-velha.component';
+
+
+
+@NgModule({
+  declarations: [JogoDaVelhaComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    JogoDaVelhaComponent
+  ]
+})
+export class JogoDaVelhaModule { }
